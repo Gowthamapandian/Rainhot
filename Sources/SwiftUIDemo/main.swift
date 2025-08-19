f0cc513d44e940c578d16f0d0ed9d02266f9e72e
Gowthamapandian <166529295+Gowthamapandian@users.noreply.github.com>
2025-08-19 14:42:21 +0530

    Create main.swift

---
+++ Sources/SwiftUIDemo/main.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello, SwiftUI Demo!")
+                .font(.title)
+                .padding()
+        }
+    }
+}
